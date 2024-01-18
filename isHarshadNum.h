0d91269d27de6775781f37ef6640511d04d72301
Joseph Semaan Sfeir <202311768@ua.edu.lb>
2024-01-18 19:04:23 +0200

    Initial Commit

---
+++ isHarshadNum.h
@@ -0,0 +1,14 @@
+//
+// Created by Joseph on 1/18/2024.
+//
+
+#ifndef ISHARSHADNUM_ISHARSHADNUM_H
+#define ISHARSHADNUM_ISHARSHADNUM_H
+
+
+class isHarshadNum {
+
+};
+
+
+#endif //ISHARSHADNUM_ISHARSHADNUM_H
